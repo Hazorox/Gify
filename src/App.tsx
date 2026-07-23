d195b6326273d27d5cd713cf58cfff6cacc6ab40
Hamza Awad <hamzaawad8899@gmail.com>
2026-07-23 16:23:17 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+
+function App() {
+  return (
+    <h1>hiiiiiii</h1>
+  );
+}
+
+export default App;
