affa2bb041bfb189bb15ae3cafd3091d25f281ee
Hamza Awad <hamzaawad8899@gmail.com>
2026-07-27 19:11:38 +0300

    added open by alt shift G, exit by Esc

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,10 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 use tauri::{
+    Manager, 
     menu::{Menu, MenuItem},
     tray::TrayIconBuilder,
 };
-
+use tauri_plugin_global_shortcut::{Code, GlobalShortcutExt, Modifiers, Shortcut, ShortcutState};
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -11,11 +12,48 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let main_key = Shortcut::new(Some(Modifiers::ALT | Modifiers::SHIFT), Code::KeyG);
+    let handler = main_key.clone();
+    let exit_key = Shortcut::new(None, Code::Escape);
     tauri::Builder::default()
-        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        .plugin(
+            tauri_plugin_global_shortcut::Builder::new()
+                // Listen for the shortcut
+                .with_handler(move |app, shortcut, event| {
+                    if shortcut == &handler {
+                        match event.state() {
+                            ShortcutState::Pressed => {
+                                println!("Pressed!");
+                                if let Some(win) = Manager::get_webview_window(app, "main") {
+                                    let _ = win.show();
+                                    let _ = win.set_focus();
+                                    let _ = win.set_visible_on_all_workspaces(true);
+                                }
+                            }
+                            _ => {}
+                        }
+                    } else if shortcut == &exit_key {
+                        match event.state() {
+                            ShortcutState::Pressed => {
+                                if let Some(win) = tauri::Manager::get_webview_window(app, "main") {
+                                    let _ = win.hide();
+                                    let _ = win.set_visible_on_all_workspaces(false);
+                                }
+                            }
+                            _ => {}
+                        }
+                    }
+                })
+                .build(),
+        )
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
-        .setup(|app| {
+        .setup(move |app: &mut tauri::App| {
+            // register the shortcut
+            app.global_shortcut().register(main_key)?;
+            app.global_shortcut().register(exit_key)?;
+
+            // Registering tray buttons and setup
             let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
             let menu = Menu::with_items(app, &[&quit_i])?;
             let _tray = TrayIconBuilder::new()
