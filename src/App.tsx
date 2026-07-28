dff9c1e20009582eb7ad0b9fc759da6605a132d7
Hamza Awad <hamzaawad8899@gmail.com>
2026-07-28 03:48:54 +0300

    base frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
+import { IoIosSettings } from "react-icons/io";
 import "./App.css";
 
 function App() {
   return (
-    <h1>hiiiiiii</h1>
+    <div className="w-full h-full bg-[#282828] text-white">
+      <div data-tauri-drag-region className="w-full border-b-2 h-[8%]  gap-2 text-xl flex flex-none justify-between px-2 py-1 items-center border-[#717171]">
+        GIFy
+        <input type="text" placeholder="Search for GIFs" className="w-[60%] placeholder:text-center flex justify-center self-center items-center border-[#fffbe6] rounded-lg sticky border-2"  />
+        <IoIosSettings />
+      </div>
+      <div className="flex-1">contents</div>
+      <div className="bottom-0 absolute w-full justify-around flex items-center flex-none h-[8%] border-t-2 border-[#717171]">Footer</div>
+    </div>
   );
 }
 
